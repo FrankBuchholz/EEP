64469513d455c53e7131b4f24df73d0825fbe948
Frank Buchholz <frank.buchholz@web.de>
2019-01-25 18:25:47 +0100

    Update EEP_Inventar.lua 18.01.2019
    Das Skript ist jetzt in zwei Teile aufgeteilt:
    
    1. das Modul EEP2Lua, das die EEP-Anlagedatei liest und konvertiert
    2. das Skript EEP_Inventar, das die Ausgabe der Ergebnisse übernimmt
    Das Modul EEP2Lua könnte, geeignet eingebunden (und ggf. geeignet erweitert), nun auch innerhalt einer normalen EEP-Anlagedatei verwendet werden, um den Betriebsstart zu beeinflussen. 
    
    
    
    Beispielweise könnte man nach dem Einsetzen von Zügen in Schattenbahnhöfe (Speichern der Anlage nicht vergessen!) dann beim Neustart der Anlage die Zugverbände mit Höhe z<0 für sein eignenes Skript ermitteln ohne dass die Züge zunächst über Kontakte fahren müssen. Möglicherweise ließe sich so die EEP-Speicherverwaltung teilweise ersetzen. Man könnte auch einfach alle Zugverbände - da man nun ja alle Namen kennt - starten oder stoppen, alle Licher an- oder ausschalten, alle Signale auf Halt stellen und dergleichen mehr. Es ließe sich also auch allerlei Unsinn anstellen, also hübsch vorsichtig vorgehen:-)
    
    
    
    Allerdings gäbe es für derlei Anwendungen das wohl schwerwiegende Hindernis, dass das Modul nur den Zustand der gespeicherten Anlage lesen kann, also nicht den aktuellen Zustand der laufenden Anlage kennt.
    
    
    
    Die Laufzeit bei größeren Anlagen ist auch nicht zu vernachlässigen. Für den Demoanlage in EEP 13 dauert es z.B. ca 12 Sekunden, bis xml2lua die 7.2 MB große Datei verearbeitet hat. (Dagegen fallen die weiteren Verarbeitungsschritte nicht ins Gewicht.)
    
    
    
    Die Neustrukturierung des Programms hat übrigens - wie zu erwarten war - deutlich mehr Zeit gekostet, als die Erstellung der ursprünglichen Version. Ein besonderer Dank geht an Christopher dessen Beitrag zur Lua-Programmierung mir viele Tipps und Ideen lieferte. (Vielleicht baue ich auch noch sein Debug-Modul ein.)
    
    
    
    Eine Weile habe ich zwischen folgenden Designmöglichkeiten geschwankt:
    
    Möglichst geringer Eingriff in die von xml2lua gelieferte Datenstruktur. Dabei hätte ich lediglich Querbeziehungen zwischen den Tags hinzufügt, so dass man leicht z.B. vom Rollmaterial über den Zugverband zu einen Gleis und damit zur Position kommt. Bei Datenzugriffen hätte das dann allerdings elendlich lange Zugriffspfade ergeben.
    Konsequenter Einsatz von get-Funktionen, die grundsätzlich auf die von xml2lua gelieferte Datenstruktur zugreifen. Alldings finde ich es eher hässlich überall im Auswertungsskript dann getXxx() schreiben zu müssen.
    Am Ende habe ich jedoch eine für den Aufrufer möglichst übersichtliche Schnittstelle gewählt, so dass vor allem das Anzeigeskript EEP_Inventar leicht zu lesen und verstehen ist . (hoffentlich...).
    Die Analyse der XML-Datenstruktur ist übrigens vermutlich komlizierter programmiert als notwendig: Das Modul xmlhandler.tree bietet einige Optionen an, mit der das Ergebnis der Konvertierung beeinflusst werden kann. Insbesondere könnte erreicht werden, dass Tags wie z.B. 'Rollmaterial', die ein oder mehrfach vorkommen können einheitlich dargestellt werden. Mit der aktuellen Version musste ich dafür noch ein paar Klimmzüge machen. 
    
    
    
    Einen Strukturierungsfehler muss ich noch korrigieren: Die Funktionen sind noch nicht alle als local deklariert - ich hatte versäumt, die aufeinander aufbauenden Funktionen in der notwendigen Reihenfolge anzugeben. Das fällt nicht weiter auf, da man globale Funktionen auch dann aufrufen kann wenn die Definition erst danach in der Datei vorkommt.
    
    
    
    Und zu guter Letzt ein Wort zum Lua-Fluch der Punkte und Kommas: Ich weiß nicht wie oft ich gespeichert und gestartet habe, nur um festzustellen das ein Punkt oder Komma zuviel oder zuwenig vorkam...

--- EEP_Inventar.lua
+++ EEP_Inventar.lua
@@ -4,31 +4,29 @@ Analyse einer EEP-Anlage-Datei
 Frank Buchholz, 2019
 ]]
 
-local _VERSION = 'v2019-01-14'
+local _VERSION = 'v2019-01-18'
 
 -- In Variable input_file wird der Pfad zur Anlagen-Datei eingetragen
 -- Hier einige Varianten unter der Annahme, dass dieses Script im LUA-Ordner der EEP-Installation liegt:
 local input_files = {
-	[1] = "./Anlagen/Tutorials/Tutorial_57_sanftes_Ankuppeln.anl3", 				-- relativer Pfad beim Start aus EEP heraus
-	[2] = "../Resourcen/Anlagen/Tutorials/Tutorial_57_sanftes_Ankuppeln.anl3", 		-- relativer Pfad bei standalone-Ausführung im Editor SciTE
-	[3] = "./Anlagen/Demo.anl3",													-- relativer Pfad beim Start aus EEP heraus
-	[4] = "../Resourcen/Anlagen/Demo.anl3"											-- relativer Pfad bei standalone-Ausführung im Editor SciTE
+	-- relativer Pfad beim Start aus EEP heraus
+	[1] = "./Anlagen/Tutorials/Tutorial_57_sanftes_Ankuppeln.anl3", 		-- Laufzeit ca 0.5 Sekunden		
+	[2] = "./Anlagen/Demo.anl3",											-- Laufzeit ca 12 Sekunden
+	[3] = "./Anlagen/Wasser Demo.anl3",	
+	-- relativer Pfad bei standalone-Ausführung im Editor SciTE
+	[4] = "../Resourcen/Anlagen/Tutorials/Tutorial_57_sanftes_Ankuppeln.anl3", 		
+	[5] = "../Resourcen/Anlagen/Demo.anl3"		
 
 }
 -- Welche Datei soll es sein?
-input_file = input_files[4]
+input_file = input_files[3]
 
 -- Hier den Pfad zur zusätzlichen Ausgabe-Datei angeben (siehe printtofile)
 local output_file = "C:/temp/output.txt"
 
--- Ausführliche Protokollierung zur Fehleranalyse (true/false)
-local log = false
 
 
-
-
--- printtofile (optional)-----------------------------------------------------
---[[
+--[[------ printtofile (optional)--------------------------------------
 Quelle:
 https://emaps-eep.de/lua/printtofile
 
@@ -44,449 +42,169 @@ if EEPVer then -- Nur notwendig wenn das Skript aus EEP heraus gestartet wird
 	require("PrintToFile_BH2"){file = output_file, output = 1}
 end
 
-if EEPVer then -- Nur möglich wenn das Skript aus EEP heraus gestartet wird
+
+
+-- Hilfsfunktion zur Sortierung einer Tabelle
+-- siehe https://www.lua.org/pil/19.3.html
+function sortedTable(tab, func)
+	local a = {}
+	for n in pairs(tab) do table.insert(a, n) end	-- copy references to table entries into local table 
+	table.sort(a, func)
+	local i = 0										-- iterator variable
+	local iter = function ()						-- iterator function
+		i = i + 1
+		if a[i] == nil then return nil
+		else return a[i], tab[a[i]]					-- return key, value
+		end
+	end
+	return iter
+end
+
+-- EEP-Anlagedatei analysieren ----------------------------------------
+
+if EEPVer then -- Nur möglich und sinnvoll wenn das Skript aus EEP heraus gestartet wird
 	clearlog()
 	print("EEP Version", " ", EEPVer, " ", "Lua Version", " ", _G._VERSION)
 else
 	print("Lua Version", " ", _G._VERSION)
 end
 
+-- Laden und Verarbeiten der EEP-Anlagen-Datei
+-- Der optionale Rückgabewert sutrackp enthält die Tabellenstruktur in der Form wie sie xml2lua liefert. 
+local Anlage 				= require('EEP2Lua')
+local sutrackp 				= Anlage.loadFile(input_file)
 
+-- statische Texte und Definitionen stehen direkt nach dem require-Befehl zur Verfügung
+local GleissystemText 		= Anlage.GleissystemText
+local Gleisart				= Anlage.Gleisart
+local GebaeudesammlungText 	= Anlage.GebaeudesammlungText
 
--- xml2lua --------------------------------------------------------------
---[[
-Quelle:
-https://github.com/manoelcampos/xml2lua
+-- Alle anderen Variablen stehen nach Aufruf von loadFile zur Verfügung 
+print('EEP2Lua Version', ' ', 			Anlage._VERSION)
 
-Benötigte Dateien im LUA-Ordner:
-xml2lua.lua
-XmlParser.lua
-xmlhandler/tree.lua
---]]
+print('Auswertung der Anlage', ' ', 	Anlage.Datei )
+print('')
 
-local xml2lua = require("xml2lua")
-local handler = require("xmlhandler.tree")
+print('Dateigroessee: ', 				math.floor(Anlage.Statistics.FileSize / 1000), ' kB')
+print('Laufzeit:')
+print('Datei laden: ', 					string.format("%.3f", Anlage.Statistics.LoadFileTime), ' sek') 
+print('XML in Lua-Tabelle umwandeln: ', string.format("%.3f", Anlage.Statistics.ParserTime),   ' sek')
+print('XML verarbeiten: ', 				string.format("%.3f", Anlage.Statistics.ProcessTime),  ' sek') 
+print('Gesamt: ', 						string.format("%.3f", Anlage.Statistics.TotalTime),    ' sek') 
 
---Load the XML file
-local xml = xml2lua.loadFile(input_file)
+print('')
+print('Beschreibung:')
+print(Anlage.Beschreibung )
+print('----')
 
---Instantiates the XML parser
-local parser = xml2lua.parser(handler)
-parser:parse(xml)
-
-
-
-
--- process --------------------------------------------------------------
-
---[[
-Eine EEP-Anlagedatei hat folgenden Aufbau:
-
-sutrackp
-	Version
-	Gleissystem (multiple: 1=Eisenbahn, 2=Straßenbahn, 3=Straße, 4=Wasserwege, 5=Steuerstrecken, 6=GBS)
-		Dreibein
-			Vektor (multiple: Pos=Position, Dir=Richtung, Nor=Normale, Bin=Binormale)
-		Gleis (multiple)
-			Dreibein
-				Vektor (multiple: Pos=Position, Dir=Richtung, Nor=Normale, Bin=Binormale)
-			Anfangsfuehrungsverdrehung
-			Charakteristik
-			Kontakt
-	Kollektor
-		Dreibein
-			Vektor (multiple)
-		Gleis (multiple)
-	Fuhrpark
-		Zugverband (multiple)
-			Gleisort
-				Rollmaterial (multiple)
-					Zugmaschine
-						Motor
-							Getriebe
-					Modell
-						Achse
-							Achse
-	Gebaeudesammlung (multiple)
-		Immobile
-			...
-	Gleisbildstellpultsammlung
-		...
-	AnimRoll
-	Goods
-	CrowdSammlung
-		...
-	Kammerasammlung
-		Kammera (multiple)
-	Settings
-	Options
-	Weather
-	TimeTable
-	EEPLua (LUAS=Prüfsumme für Lua-Datei, LUAPath=Dateiname)
-	Kamera3D
-	Schandlaft
-	Beschreibung
-
---]]
-
-local Anlage = handler.root.sutrackp
-
-local GleissystemText = { -- GleissystemID
-	[1] = 'Eisenbahn',
-	[2] = 'Straßenbahn',
-	[3] = 'Straße',
-	[4] = 'Wasserwege',
-	[5] = 'Steuerstrecken',
-	[6] = 'GBS'
-}
-
-local Gleisart = { -- clsid
-	['2E25C8E2-ADCD-469A-942E-7484556FF932'] = 'Normales Gleis',
-	['C889EADB-63B5-44A2-AAB9-457424CFF15F'] = '2-Weg-Weiche',
-	['B0818DD8-5DFD-409F-8022-993FD3C90759'] = '3-Weg-Weiche',
-	['06D80C90-4E4B-469B-BFE0-509A573EBC99'] = 'Prellbock-Gleis'
-}
-
-local GebaeudesammlungText = { -- GebaudesammlungID
-	[1] = 'Gleisobjekte-Gleise',
-	[2] = 'Gebaeudesammlung 2',
-	[3] = 'Gleisobjekte-Strassen',
-	[4] = 'Immobilien',
-	[5] = 'Landschaftselemente',
-	[6] = 'Gebaeudesammlung 6'
-}
-
--- Position der Gleise:
--- Gleise[GleissystemID]{Anzahl, Laenge}
--- Gleise[GleissystemID][GleisID]{x,y}
-local Gleise = {}
-
--- Position der Immobilen:
--- Immobilen[GebaeudesammlungID]{Anzahl}
--- Immobilen[GebaeudesammlungID][ImmoIdx]{x,y}
-local Immobilen = {}
-
-
--- Übersicht zu referenzierten Dateien:
--- Dateien[Typ][Datei]{Anzahl}
-local Dateien = {}
-Dateien['Gleissystem']  = {}
-Dateien['Rollmaterial'] = {}
-Dateien['Immobile']     = {}
-
--- Hilfsfunktion zur Prüfung ob eine Tabelle einen bestimmten Eintrag enthält
-function tableHasKey(table,key)
-    return table[key] ~= nil
-end
-
--- Hilfsfunktion zur sortierung einer Tabelle
--- siehe https://www.lua.org/pil/19.3.html
-function sortedTable(t, f)
-	local a = {}
-	for n in pairs(t) do table.insert(a, n) end
-	table.sort(a, f)
-	local i = 0					-- iterator variable
-	local iter = function ()	-- iterator function
-		i = i + 1
-		if a[i] == nil then return nil
-		else return a[i], t[a[i]]
-		end
-	end
-	return iter
-end
-
-function processAnlage(Anlage)
-	for key, value in pairs(Anlage) do
-
-		if     key == 'Beschreibung' then
-			--print("Anlage Beschreibung: ", value)
-
-		elseif  key == 'Gleissystem' then
-			--print('Ag ', key, ' ', type(value))
-			processGleissystem(value)
-
-		elseif  key == 'Fuhrpark' then
-			--print('Af ', key, ' ', type(value))
-			processFuhrpark(value)
-
-		elseif  key == 'Gebaeudesammlung' then
-			--print('Af ', key, ' ', type(value))
-			processGebaeudesammlung(value)
-
-		else
-			--print('Ax ', key, ' ', type(value))
-
-		end
-	end
-
-	print("")
-	print("Gleissysteme:")
-	for key, value in pairs(Gleise) do
-		print(GleissystemText[tonumber(key)], ' Anzahl=', value.Anzahl, ' Länge=', value.Laenge/100, 'm')
-	end
-
-	print("")
-	print("Gebaeudesammlung:")
-	for key, value in pairs(Immobilen) do
-		print(GebaeudesammlungText[tonumber(key)], ' Anzahl=', value.Anzahl)
-	end
-
-	for key, value in pairs(Dateien) do
-		print("")
-		print("Dateien ", key, ':')
-		for key, value in sortedTable(value) do
-			print(key, ' Anzahl=', value)
-		end
-	end
-
+print('EEP Version der Anlage', ': ', 	Anlage.EEP )
+if Anlage.No3DMs then 		-- Das Attribut gibt ab EEP 13
+	print('Anzahl der Resourcen',   ': ', Anlage.No3DMs )
 end
 
 
-function processGleissystem(Gleissystem)
-	if not type(Gleissystem) == 'table' then
-		print('GSv ', Gleissystem)
+-- Ausgabe der Informationen zur Anlage 
+-- Das Komma am Anfang der Zeilen innerhalb von print erleichtert es, einzelne Zeilen auszukommentieren.
 
-	elseif tableHasKey(Gleissystem, '_attr') then 		-- Single entry
-		if log then print('GSa ', type(Gleissystem)) end
-
-		local GleissystemID = tonumber(Gleissystem._attr.GleissystemID)
-
-		Gleise[GleissystemID] = {}
-		Gleise[GleissystemID].Anzahl = 0
-		Gleise[GleissystemID].Laenge = 0
-
-		if log then
-			print("")
-			print('Gleissystem ', GleissystemID, ': ', GleissystemText[GleissystemID])
-		end
-
-		for key, value in pairs(Gleissystem) do
-			if key == 'Gleis' then
-				processGleis(GleissystemID, value)
-			end
-		end
-
-	else											--	Multiple entries
-		if log then print('G- ', type(Gleissystem)) end
-		for key, value in pairs(Gleissystem) do
-			if log then print('GS ', key, ' ', type(value)) end
-			processGleissystem(value)
-		end
+print('')
+print('Gleise:')
+for GleissystemID, Gleissystem in pairs(Anlage.Gleise) do
+	if Gleissystem.Anzahl > 0 then 
+	print(
+		  GleissystemText[GleissystemID], ' '
+		, 'Anzahl=', 		Gleissystem.Anzahl, ' '
+		, 'Laenge=', 		Gleissystem.Laenge, 'm' 
+	)
 	end
 end
 
-
-function processGleis(GleissystemID, Gleis)
-	if not type(Gleis) == 'table' then
-		print('Gv ', Gleis)
-
-	elseif tableHasKey(Gleis, '_attr') then 		-- Single entry
-		if log then print('Ga ', type(Gleis)) end
-
-		local GleisID = tonumber(Gleis._attr.GleisID)
-
-		-- Speichere Position des Gleises
-		Gleise[GleissystemID][GleisID] = { ['x'] = Gleis.Dreibein.Vektor[1]._attr.x, ['y'] = Gleis.Dreibein.Vektor[1]._attr.y }
-		Gleise[GleissystemID].Anzahl = Gleise[GleissystemID].Anzahl + 1
-		Gleise[GleissystemID].Laenge = Gleise[GleissystemID].Laenge + tonumber(Gleis.Charakteristik._attr.Laenge)
-
-		-- Speichere Datei zum Gleis
-		if Dateien['Gleissystem'][Gleis._attr.gsbname] then
-			Dateien['Gleissystem'][Gleis._attr.gsbname] = Dateien['Gleissystem'][Gleis._attr.gsbname] + 1
-		else
-			Dateien['Gleissystem'][Gleis._attr.gsbname] = 1
-		end
-
-		if log then
-			print(
-				'Gleis ', GleisID, ': ',
-				'x=', Gleis.Dreibein.Vektor[1]._attr.x, ' y=', Gleis.Dreibein.Vektor[1]._attr.y, ' ',
-				'l=', Gleis.Charakteristik._attr.Laenge
-			)
-		end
-
-	else											--	Multiple entries
-		if log then print('G- ', type(Gleis)) end
-		for key, value in pairs(Gleis) do
-			if log then print('G ', key, ' ', type(value)) end
-			processGleis(GleissystemID, value)
-		end
-	end
-end
-
-
-function processFuhrpark(Fuhrpark)
-	if not type(Fuhrpark) == 'table' then
-		print('Fv ', Fuhrpark)
-
-	elseif tableHasKey(Fuhrpark, '_attr') then 		-- Single entry
-		if log then print('Fa ', type(Fuhrpark)) end
-
-		print('Fuhrpark ', Fuhrpark._attr.FuhrparkID, ': ')
-
-		for key, value in pairs(Fuhrpark) do
-			if key == 'Zugverband' then
-				processZugverband(value)
-			end
-		end
-
-	else											--	Multiple entries
-		if log then print('F- ', type(Fuhrpark)) end
-		for key, value in pairs(Fuhrpark) do
-			print('F ', key, ' ', type(value))
-			processFuhrpark(value)
-		end
-	end
-end
-
-function processZugverband(Zugverband)
-	if not type(Zugverband) == 'table' then
-		print('Zv ', Zugverband)
-
-	elseif tableHasKey(Zugverband, '_attr') then 	-- Single entry
-		if log then print('Za ', type(Zugverband)) end
-
-		local GleissystemID = tonumber(Zugverband.Gleisort._attr.gleissystemID)
-		local GleisID       = tonumber(Zugverband.Gleisort._attr.gleisID)
-
-		print("")
+print('')
+--print('Fuhrparks:')
+for FuhrparkID, Fuhrpark in pairs(Anlage.Fuhrparks) do
+	print('Fuhrpark ',	FuhrparkID, ':')
+	if Fuhrpark.Zugverbaende.Anzahl > 0 then 
 		print(
-			'Zugverband ', 	Zugverband._attr.ZugID, ': ', Zugverband._attr.name, ' ',
-			'Gleisort: ',   GleissystemText[GleissystemID], ' ', GleisID, ' ',
-			'x=', tonumber(Gleise[GleissystemID][GleisID].x)/100, 'm ',
-			'y=', tonumber(Gleise[GleissystemID][GleisID].y)/100, "m"
+			'Zugverbaende: ', 		'Anzahl=', Fuhrpark.Zugverbaende.Anzahl
 		)
-
-		for key, value in pairs(Zugverband) do
-			if key == 'Rollmaterial' then
-				processRollmaterial(value)
-			end
-		end
-
-	else											--	Multiple entries
-		if log then print('Z- ', type(Zugverband)) end
-		for key, value in pairs(Zugverband) do
-			if log then print('Z ', key, ' ', type(value)) end
-			processZugverband(value)
-		end
+	end
+	if Fuhrpark.Rollmaterialien.Anzahl > 0 then 
+		print(
+			'Rollmaterialien: ', 	'Anzahl=', Fuhrpark.Rollmaterialien.Anzahl
+		)
 	end
 end
 
-function processRollmaterial(Rollmaterial)
-	if not type(Rollmaterial) == 'table' then
-		print('Rv ', Rollmaterial)
-
-	elseif tableHasKey(Rollmaterial, '_attr') then 	-- Single entry
-		if log then print('Ra ', type(Rollmaterial)) end
-
-		local Zugmaschine = tableHasKey(Rollmaterial, 'Zugmaschine')
-
-		if Zugmaschine then
-			print('Zugmaschine: ', " ",  Rollmaterial._attr.name, "        \t", Rollmaterial._attr.typ)
-		else
-			print('Rollmaterial: ', " ", Rollmaterial._attr.name, "        \t", Rollmaterial._attr.typ)
-		end
-
-		-- Speichere Datei zum Rollmaterial
-		if Dateien['Rollmaterial'][Rollmaterial._attr.typ] then
-			Dateien['Rollmaterial'][Rollmaterial._attr.typ] = Dateien['Rollmaterial'][Rollmaterial._attr.typ] + 1
-		else
-			Dateien['Rollmaterial'][Rollmaterial._attr.typ] = 1
-		end
-
-	else											--	Multiple entries
-		if log then print('R- ', type(Rollmaterial)) end
-		for key, value in pairs(Rollmaterial) do
-			if log then print('R ', key, ' ', type(value)) end
-			processRollmaterial(value)
-		end
+print('')
+print('Immobilien:')
+for GebaeudesammlungID, Gebaeudesammlung in pairs(Anlage.Immobilien) do
+	if Gebaeudesammlung.Anzahl > 0 then 
+	print(
+--		  GebaeudesammlungID, ' ',
+		  GebaeudesammlungText[GebaeudesammlungID], ' '
+		, 'Anzahl=', 		Gebaeudesammlung.Anzahl
+	)
 	end
 end
 
-function processGebaeudesammlung(Gebaeudesammlung)
-	if not type(Gebaeudesammlung) == 'table' then
-		print('ISv ', Gebaeudesammlung)
+print('')
+print('Zugverbaende:')
+-- Die Tabelle der Zugverbände verwendet einen numerischen Schlüssel, daher kann pairs verwendet werden
+for ZugID, Zugverband in pairs(Anlage.Zugverbaende) do
+	print('')
+	print(
+		  'Zugverband ', 	ZugID, ': ', Zugverband.name, ' '
+		, 'Gleisort: ', 	GleissystemText[Zugverband.GleissystemID], ' '
+		, 'Gleis: ', 		Zugverband.GleisID,  ' '
+		, 'x=' ..			Zugverband.Position.x .. 'm', ' '
+		, 'y=' .. 			Zugverband.Position.y .. 'm', ' '
+		, ( Zugverband.Position.z ~= 0 and 'z=' .. Zugverband.Position.z .. 'm' or ''), ' '
+		, ( Zugverband.Geschwindigkeit ~= 0 and 'v=' .. Zugverband.Geschwindigkeit .. 'km/h' or ''), ' '
+	)
+	for Nr, Rollmaterial in pairs(Zugverband.Rollmaterialien) do
+		print(
+--			Nr, ' ', 
+			  Rollmaterial.name, ' '
+			, ( Rollmaterial.Zugmaschine and 'Zugmaschine' .. ' ' or '' )
+--			, 'Datei: ', Rollmaterial.typ
+		)
+	end
+	
+end
 
-	elseif tableHasKey(Gebaeudesammlung, '_attr') then 		-- Single entry
-		if log then print('ISa ', type(Gebaeudesammlung)) end
+print('')
+print('Rollmaterialien:')
+-- Die Tabelle der Rollmaterialien verwendet den Namen als Schlüssel. 
+-- Für eine sortierte Anzeige muss die Tabelle erst sortiert werden.
+for name, Rollmaterial in sortedTable(Anlage.Rollmaterialien) do
+	print(
+		  name, ' '
+		, ( Rollmaterial.Zugmaschine and 'Zugmaschine' or 'Rollmaterial' ), ' '
+		, 'Zugverband: ', 	Rollmaterial.ZugID, ' ', Rollmaterial.Zugverband.name, ' '
+		, 'x=' .. 			Rollmaterial.Zugverband.Position.x .. 'm', ' '
+		, 'y=' .. 			Rollmaterial.Zugverband.Position.y .. 'm', ' '
+		, ( Rollmaterial.Zugverband.Position.z ~= 0 and 'z=' .. Rollmaterial.Zugverband.Position.z .. 'm' or ''), ' '
+		, 'Datei: ', 		Rollmaterial.typ
+	)
+end
 
-		local GebaeudesammlungID = Gebaeudesammlung._attr.GebaudesammlungID
-
-		Immobilen[GebaeudesammlungID] = {}
-		Immobilen[GebaeudesammlungID].Anzahl = 0
-
-		if log then
-			print("")
-			print('Gebaeudesammlung ', GebaeudesammlungID, ':')
-		end
-
-		for key, value in pairs(Gebaeudesammlung) do
-			if key == 'Immobile' then
-				processImmobile(GebaeudesammlungID, value)
-			end
-		end
-
-	else											--	Multiple entries
-		if log then print('G- ', type(Gebaeudesammlung)) end
-		for key, value in pairs(Gebaeudesammlung) do
-			if log then print('GS ', key, ' ', type(value)) end
-			processGebaeudesammlung(value)
-		end
+-- Die Dateitabelle enthält die Unterkategorien 'Gleissystem', 'Rollmaterial' und 'Immobile'
+-- Daher muss die Dateitabelle zweistufig durchlaufen werden 
+print('')
+print('Dateien:')
+for Typ, Dateiliste in pairs(Anlage.Dateien) do
+	print("")
+	print("Dateien ", Typ, ':')
+	for Dateiname, Datei in sortedTable(Dateiliste) do
+		print(
+			  Dateiname 
+			, ' Anzahl=', 	Datei.Anzahl, ' '
+			, 'x=' .. 		Datei.Positionen[1].x .. 'm', ' '	-- bis zu 10 Positionen stehen zur Verfügung
+			, 'y=' .. 		Datei.Positionen[1].y .. 'm', ' '	-- davon zeigen wir hier nur die erste Postion
+			, ( Datei.Positionen[1].z ~= 0 and 'z=' .. Datei.Positionen[1].z .. 'm' or ''), ' '
+			, ( #Datei.Positionen > 1 and '...' or '' )
+	)
 	end
 end
-
-
-function processImmobile(GebaeudesammlungID, Immobile)
-	if not type(Immobile) == 'table' then
-		print('Iv ', Immobile)
-
-	elseif tableHasKey(Immobile, '_attr') then 		-- Single entry
-		if log then print('Ga ', type(Immobile)) end
-
-		local ImmoIdx = Immobile._attr.ImmoIdx
-
-		-- Speichere Position der Immobile
-		Immobilen[GebaeudesammlungID][ImmoIdx] = { ['x'] = Immobile.Dreibein.Vektor[1]._attr.x, ['y'] = Immobile.Dreibein.Vektor[1]._attr.y }
-		Immobilen[GebaeudesammlungID].Anzahl = Immobilen[GebaeudesammlungID].Anzahl + 1
-
-		-- Speichere Datei zum Immobile
-		if Dateien['Immobile'][Immobile._attr.gsbname] then
-			Dateien['Immobile'][Immobile._attr.gsbname] = Dateien['Immobile'][Immobile._attr.gsbname] + 1
-		else
-			Dateien['Immobile'][Immobile._attr.gsbname] = 1
-		end
-
-		if log then
-			print(
-				'Immobile ', ImmoIdx, ': ',
-				'x=', Immobile.Dreibein.Vektor[1]._attr.x, ' y=', Immobile.Dreibein.Vektor[1]._attr.y, ' '
-			)
-		end
-
-	else											--	Multiple entries
-		if log then print('I- ', type(Immobile)) end
-		for key, value in pairs(Immobile) do
-			if log then print('I ', key, ' ', type(value)) end
-			processImmobile(GebaeudesammlungID, value)
-		end
-	end
-end
-
-
--- Start ----------------------------------------------------------------
-
-print("Auswertung der Anlage", " ", input_file)
---print(Anlage.Beschreibung)
-print("")
-
-print("EEP Version der Anlage", ": ", Anlage.Version._attr.EEP)
-if Anlage.Version._attr.No3DMs then 		-- Das Attribut gibt es in alten EEP-Versionen noch nicht 
-	print("Anzahl der Resourcen",   ": ", Anlage.Version._attr.No3DMs)
-end
-print("")
-
--- Los geht's
-processAnlage(Anlage)
 
 
 -- EEPMain --------------------------------------------------------------
